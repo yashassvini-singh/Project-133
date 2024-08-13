c7aa4a48b5e562e06af274b8cd8814bdc0b8cc30
yashassvini-singh <152004611+yashassvini-singh@users.noreply.github.com>
2024-08-13 19:39:39 +0530

    Add files via upload

---
+++ TV.js
@@ -0,0 +1,44 @@
+status = "";
+tv_image = "";
+objects=[];
+
+function preload(){
+    tv_image = loadImage("TV.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(tv_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(tv_image,0,0,640,350);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 14, objects[i].y - 175);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);
+        }
+    }
+}
